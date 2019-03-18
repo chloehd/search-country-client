3f3af642eec6f6a28d2729c4c6c4a46f74ff5c2d
chloehd <chloehipeau@gmail.com>
2019-03-18 18:59:05 +0100

    clear app.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,6 @@ import Search from "./Components/Search.js";
 import './App.css';
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     myCountry: countries,
-  //     searchString: ""
-  //   }
-  // }
-
-
-  // handleSearch(event){
-  //   const {value} = event.target;
-  //   const filteredArray = countries.filter(
-  //       (oneCountry)=> oneCountry.name.toLowerCase().includes(value.toLowerCase())
-  //       )
-  //   this.setState({searchString: value, myCountry: filteredArray})
-  // }
-
 
   render() {
     return (
